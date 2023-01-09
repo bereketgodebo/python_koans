bbf998488c551ad7360fba0bf135d1237f5f2c53
Bereket Godebo <bereket.godebo@outlook.com>
2023-01-09 12:42:40 +0300

    lesson exception and triangleProject complete

--- koans/triangle.py
+++ koans/triangle.py
@@ -17,8 +17,18 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    if a <= 0 or b <= 0 or c <= 0:
+        raise TriangleError
+    elif a + b < c or a + c < b or b + c < a:
+        raise TriangleError
+        
+    if a == b and a == c and b == c: # first check if all sides are equal
+        return 'equilateral'
+    elif  a == b or a == c or b == c: # then check if any two sides are equal
+        return 'isosceles'
+    else:
+        return 'scalene' # then we can conclude none is equal
+    
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
