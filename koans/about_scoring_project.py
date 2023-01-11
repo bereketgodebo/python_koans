f00c4ed3fa95b9bfa13be49c7a4678b83d15bffc
Bereket Godebo <bereket.godebo@outlook.com>
2023-01-12 00:06:24 +0300

    lesson scoringproject complete

--- koans/about_scoring_project.py
+++ koans/about_scoring_project.py
@@ -3,6 +3,8 @@
 
 from runner.koan import *
 
+import collections
+
 # Greed is a dice game where you roll up to five dice to accumulate
 # points.  The following "score" function will be used to calculate the
 # score of a single roll of the dice.
@@ -33,8 +35,32 @@ from runner.koan import *
 # Your goal is to write the score method.
 
 def score(dice):
-    # You need to write this method
-    pass
+    # get the frequency of the numbers
+    total_points = 0
+    element_frequency = collections.Counter(dice)
+    for number, frequency in element_frequency.items():
+        # for a case where there is a set of three
+        if frequency >= 3 and number == 1:
+            # three set of ones => 1000 points
+            #A one (that is not part of a set of three) is worth 100 points.
+            total_points += 1000 + (frequency - 3) * 100
+
+        elif frequency >= 3:
+            #A set of three numbers (other than ones) is worth 100 times the number
+            total_points += number * 100
+            if number == 5:
+                #A five (that is not part of a set of three) is worth 50 points.
+                total_points += (frequency - 3) * 50
+        # for a case where there is no set of three
+        else:
+            if number == 1:
+                total_points += frequency * 100
+
+            elif number == 5:
+                total_points += frequency * 50
+
+    return total_points
+
 
 class AboutScoringProject(Koan):
     def test_score_of_an_empty_list_is_zero(self):
